e0e0822af2f29f892748f02463bce44ff945a652
gmzseverr <gmzsever12@gmail.com>
2025-10-06 01:23:54 +0300

    ..

--- src/app/products/code/page.js
+++ src/app/products/code/page.js
@@ -1,10 +1,13 @@
 "use client";
 
 import ProductList from "@/components/ProductList";
+import { useParams } from "next/navigation";
 
 import React from "react";
 
 function ProductsPage() {
+  const params = useParams();
+  const type = params.type;
   return (
     <div>
       <ProductList />
